67a08e982c66f286be44a544fa1333db51fb1c10
chalodriver <chalodriver19@gmail.com>
2025-09-23 23:45:06 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,12 @@
-const CACHE_NAME = 'alfido-v1';
-const ASSETS = [
-  '/',
-  '/style1.css','/output.css',
-  '/img/logo.png','/img/logo1.png','/img/logon.png'
-];
-
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))));
-  self.clients.claim();
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
-  );
+    // Just fetch from the network, no caching
+    event.respondWith(fetch(event.request));
 });
